0ffedccfc2f9feeb70815373d7c6c5a4eac54ca0
Akesh kumar <155313882+akeshai@users.noreply.github.com>
2026-06-24 08:25:56 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,18 @@
+import random
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        combined_string = str(strs)     
+        encoded_str = []
+        for i,char in enumerate(combined_string):
+            encoded_str.extend([str(ord(char)),'[sep]'])
+        return "".join(encoded_str)
+
+
+    def decode(self, s: str) -> List[str]:
+        combined_string = []
+        for char in s.split("[sep]")[:-1]:
+            combined_string.append(chr(int(char)))
+        return eval(''.join(combined_string))
+            
+
